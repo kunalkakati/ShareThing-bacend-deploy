a084e82f0c8513b6facbef728aa84dac85fc5b84
kunalkakati <kunalkakati@2003@gmail.com>
2023-02-01 12:50:50 +0530

    blog not uploaded when image not selected bug fixed

--- routes/blog.js
+++ routes/blog.js
@@ -56,8 +56,12 @@ router.post("/add_blogs", FatchUser ,upload.single("file"), [body('description',
         // JS destructuring
         const { title, description, tags } = req.body;
         let user = await User.findOne({_id: req.user.id});
-        if (req.file === undefined) return res.send("you must select a file.");
-        const imgUrl = `https://sharething-api.onrender.com/api/blog/image/${req.file.filename}`;
+        // if (req.file === undefined) return res.send("you must select a file.");
+        const imgUrl = '';
+        if(req.file !== undefined){
+             imgUrl = `https://sharething-api.onrender.com/api/blog/image/${req.file.filename}`;
+        }
+
         // save to database[title means - title: title (if both name are same)].
         const note = new Blogs({
             user: req.user.id,
